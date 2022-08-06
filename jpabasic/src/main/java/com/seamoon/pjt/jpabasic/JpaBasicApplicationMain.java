6f5dee261d3c6df316e244e99d96a8bae1d78afa
seamoon <seamoon@seamoonui-MacBookAir.local>
2022-08-06 11:03:10 +0900

    Initial commit

---
+++ jpabasic/src/main/java/com/seamoon/pjt/jpabasic/JpaBasicApplicationMain.java
@@ -0,0 +1,7 @@
+package com.seamoon.pjt.jpabasic;
+
+public class JpaBasicApplicationMain {
+    public static void main(String[] args) {
+        System.out.println("JpaBasicApplicationMain.class");
+    }
+}
