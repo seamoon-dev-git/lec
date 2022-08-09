2613a4c11d0691f2cabb7a41fe005a3cd6e01154
seamoon <seamoon.1029@gmail.com>
2022-08-09 14:36:56 +0900

    Spring Security가 없는 상태 기본 클래스 Principal 이 NullPointerException 발생하는 문제 이슈

---
+++ springsecurity/src/main/java/com/seamoon/pjt/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.seamoon.pjt.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
